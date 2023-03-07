f26d22b88db00c2aeb06e0710bf3db36c631fbf4
Tobi <tobigta6@gmail.com>
2023-03-07 19:40:51 +0100

    changes

--- display_image.php
+++ display_image.php
@@ -18,7 +18,7 @@ $result = mysqli_query($db, "SELECT image_path FROM profile_images WHERE user_id
 // Das erste Ergebnis der Abfrage in einem Array speichern
 $image_path = mysqli_fetch_array($result);
 
-// Den Inhalt des Bildes in den HTTP-Header des Antwortschreibens einfügen
+// Den Inhalt des Bildes in den HTTP-Header des Antwortschreibens einfügenss
 header("Content-type: image/jpeg");
 
 // Das Bild ausgeben
